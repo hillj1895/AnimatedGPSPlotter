889a4fa8e5f55813211348a928043d38a31734e3
Joseph Hill <joh13010@uconn.edu>
2017-08-14 11:01:30 -0400

    Program now properly works forwards and backwards. Still need to address some issues with the looping feature as well as with the flyTo function.

--- mapMaker.js
+++ mapMaker.js
@@ -1,6 +1,7 @@
 var viewer = new Cesium.Viewer('cesiumDiv')
 var timeline = viewer.timeline;
 var clock = viewer.clock;
+var previousPoint, currentPoint, nextPoint;
 
 var data;
 
@@ -40,6 +41,47 @@ function setTimelineInterval()
   clock.currentTime = firstTimeCesium;
   clock.clockRange = Cesium.ClockRange.LOOP_STOP;
   timeline.updateFromClock();
+  previousPoint = data[0];
+  currentPoint = data[1];
+  nextPoint = data[2];
+  assignIndices();
+  console.log(nextPoint.myIndex);
+  viewer.clock.onTick.addEventListener(function() {
+    var currentTime = clock.currentTime;
+    // if current time > time in current data point -> then time increasing
+    if(currentTime > Cesium.JulianDate.fromDate(new Date(currentPoint.sense_time))) {
+      // if current time > next point -> fly to next point and change prev, current, and next configuration
+      if(currentTime > Cesium.JulianDate.fromDate(new Date(nextPoint.sense_time))) {
+        previousPoint = currentPoint;
+        nextpointIndex = currentPoint.myIndex + 1;
+        currentPoint = nextPoint;
+        nextPoint = data[nextpointIndex];
+        flyToPoint();
+      }
+    }
+    // else -> time is decreasing
+    else if(currentTime < Cesium.JulianDate.fromDate(new Date(currentPoint.sense_time || previousPoint!=null))) {
+      // if current time < prev point -> fly to prev point and change prev, current, and next configuration
+      if(currentTime < Cesium.JulianDate.fromDate(new Date(previousPoint.sense_time))) {
+        previousPointIndex = previousPoint.myIndex - 1;
+        nextPoint = currentPoint;
+        currentPoint = previousPoint;
+        previousPoint = data[previousPointIndex];
+        flyToPoint();
+      }
+
+    }
+      
+    
+    console.log(currentTime);
+  });
+}
+
+function assignIndices()
+{
+  for (var i = 0; i < data.length; i++) {
+    data[i].myIndex = i;
+  };
 }
 
     //var metaLatitude = document.getElementById("metaLatitude");
@@ -47,26 +89,32 @@ function setTimelineInterval()
 
     //metaLatitude.innerHTML += " " + citizensBankPark.position.getValue().y;
     //metaLongitude.innerHTML += " " + citizensBankPark.position.getValue().x;
+function flyToPoint() {
+  viewer.entities.removeAll();
+  viewer.entities.add({
+    position : Cesium.Cartesian3.fromDegrees(currentPoint.longitude, currentPoint.latitude),
+    point : {
+      pixelSize : 5,
+      color : Cesium.Color.RED,
+      outlineColor : Cesium.Color.WHITE,
+      outlineWidth : 2
+    }
+  });
 
-function dataCallback() {
-	// To be called each tick to check whether new point should be displayed
-	console.log('hello');
+  viewer.flyTo(viewer.entities);
 }
 
 // Sample point to fly to
-var citizensBankPark = viewer.entities.add({
-	position : Cesium.Cartesian3.fromDegrees(-73.6440, 43.3095),
-	point : {
-	    pixelSize : 5,
-		color : Cesium.Color.RED,
-		outlineColor : Cesium.Color.WHITE,
-		outlineWidth : 2
-  }
-});
-
-viewer.flyTo(viewer.entities);
-
-
+// var citizensBankPark = viewer.entities.add({
+// 	position : Cesium.Cartesian3.fromDegrees(-73.6440, 43.3095),
+// 	point : {
+//     pixelSize : 5,
+// 		color : Cesium.Color.RED,
+// 		outlineColor : Cesium.Color.WHITE,
+// 		outlineWidth : 2
+//   }
 // });
+
+//viewer.flyTo(viewer.entities);
 
 
